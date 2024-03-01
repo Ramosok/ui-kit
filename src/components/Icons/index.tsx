15e6e8ea572d6dfe77e63ba018851cbab217c9a8
Vitaliy <ramosok86@gmail.com>
2024-03-01 11:21:20 +0300

    feat: create ui-kit lib

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,19 @@
+import React, { HTMLProps, FC } from "react";
+
+import { ReactComponent as CheckIconComponent } from "../../core/assets/icons/check.svg";
+import { ReactComponent as QuestionMarkIconComponent } from "../../core/assets/icons/question-mark.svg";
+import { ReactComponent as UserIconComponent } from "../../core/assets/icons/user.svg";
+
+export type IconProps = Partial<HTMLProps<SVGElement>>;
+
+const DEFAULT_SIZE = 25;
+
+const createIcon =
+  (Icon: FC<IconProps>): FC<IconProps> =>
+  (props: IconProps) => (
+    <Icon width={DEFAULT_SIZE} height={DEFAULT_SIZE} {...props} />
+  );
+
+export const CheckIcon = createIcon(CheckIconComponent);
+export const QuestionMarkIcon = createIcon(QuestionMarkIconComponent);
+export const UserIcon = createIcon(UserIconComponent);
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import './core/styles/global.scss'
+
+export * from './components'
